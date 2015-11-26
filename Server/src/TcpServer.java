9e582024569647715bf87f6281fc9229ce5c86b9
Hogeang <jeff8080@naver.com>
2015-11-26 16:25:35 +0900

    Basement

--- Server/src/TcpServer.java
+++ Server/src/TcpServer.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TcpServer {
-dfafds;
-sdjlgajls;
+	private static Thread thread = null;
+	private static ServerSocket serverSocket = null;
+	private static Socket clientSocket = null;
+	private static final int PORT = 9193; // can use 9193 9194 9195
+	private static ThreadTcp threadServer = null;
+	
+	public static void main(String[] args) throws IOException{
+		while(true)
+		{
+			serverSocket = new ServerSocket(PORT);
+			serverSocket.setReuseAddress(true);
+			
+			while(true) {
+				clientSocket = serverSocket.accept();
+				if(clientSocket != null){
+					threadServer = new ThreadTcp(clientSocket, true);
+					thread = new Thread(threadServer);
+					thread.start();
+					clientSocket = null;
+				}
+			}
+		}	
+	}
 }
